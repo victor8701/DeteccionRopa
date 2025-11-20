d8f72a4eb70787e91fa9e98565232ca18b3dacdd
Victor8701 <100428714@alumnos.uc3m.es>
2025-11-20 20:15:40 +0100

    Traducido a python: - BorderSobel.cpp, .h y main.cpp eliminados - DeteccionRopa.py añadido (mismo contenido que lo eliminado) - Carpeta images movida al mismo nivel de src - build en .gitignore - carpeta devel CMakeLists.txt, fichero .sln y mas cosas innecesarias eliminadas

--- src/BorderSobel.h
+++
@@ -1,9 +0,0 @@
-#ifndef DETECCIONROPA_H
-#define DETECCIONROPA_H
-
-// Declaración de la función que carga y muestra la imagen
-void ChargeImage();
-void BorderSobel();
-void DetectarEsquinasCaja();
-
-#endif // DETECCIONROPA_H #pragma once
